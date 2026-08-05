69b7adfc7b9bb9225100fd84790c2c42a019ba7b
Jaivardhan Singh <jaivardhansingh050208@gmail.com>
2026-08-05 09:47:57 +0530

    Initialize Gambit Engine project

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std ;
+
+
+    class board {
+        private :
+        char board[8][8] ;
+
+        public:
+
+
+            void intitalizeBoard{
+                // To be implemented
+            }
+
+            void displayBoard{
+                // To be implemented
+            }
+    }
+int main(){
+
+    cout<<"Welcome to the Gambit Engine!" ;
+    
+}
